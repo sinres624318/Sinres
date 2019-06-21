30beebdf1a08717bb8e5b7e986b504f41b4df969
XiaoMing <fx740068892@gmail.com>
2019-06-21 09:43:34 +0800

    vant组件库配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
   <div id="app">
-
+    <GoodsAction>
+      <GoodsActionIcon
+        icon="chat-o"
+        text="客服"
+        @click="onClickIcon"
+      />
+      <GoodsActionButton
+        icon="cart-o"
+        text="购物车"
+        @click="onClickIcon"
+      />
+      <GoodsActionButton
+        type="warning"
+        text="加入购物车"
+        @click="onClickButton"
+      />
+      <GoodsActionButton
+        type="danger"
+        text="立即购买"
+        @click="onClickButton"
+      />
+    </GoodsAction>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import {
+    GoodsAction,
+    GoodsActionIcon,
+    GoodsActionButton
+  } from 'vant';
+
+
+  export default {
+    name: 'App',
+    components:{
+      GoodsAction,
+      GoodsActionIcon,
+      GoodsActionButton,
+    },
+    methods: {
+      onClickIcon() {
+        Toast('点击图标');
+      },
+      onClickButton() {
+        Toast('点击按钮');
+      }
+    }
+  }
 </script>
 
 <style>
