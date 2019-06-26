a4b1972555f6dbcdab850b2b37b38771ac3887bc
XiaoMing <fx740068892@gmail.com>
2019-06-26 17:55:49 +0800

    订单页面完成一部分  操作人---付欣

--- src/components/order/orderItem.vue
+++ src/components/order/orderItem.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="order-item">
     <OrderTitle></OrderTitle>
+    <OrderContent></OrderContent>
+    <OrderButton></OrderButton>
   </div>
 </template>
 
 <script>
   import OrderTitle from './orderTitle'
+  import OrderContent from './orderContent'
+  import OrderButton from './orderButton'
+
   export default {
     name: "orderItem",
-    components:{
-      OrderTitle
+    components: {
+      OrderTitle,
+      OrderContent,
+      OrderButton
     }
   }
 </script>
