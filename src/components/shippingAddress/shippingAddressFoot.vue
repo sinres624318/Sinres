86fc5ddf5f191eaced8a2b836799a0bb22e4406e
张明星 <l>
2019-07-02 19:18:06 +0800

    操作人--张明星 2019-07-02

---
+++ src/components/shippingAddress/shippingAddressFoot.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link  class="shippingAddressFoot" to="/newAddress" tag="div">
+    新增收货地址
+  </router-link>
+</template>
+
+<script>
+    export default {
+        name: "shippingAddressFoot"
+    }
+</script>
+
+<style scoped>
+
+</style>
